45c1f58c9d92d559390a913f3c54e8891237b09f
sirswamahesh <157932821+sirswamahesh@users.noreply.github.com>
2024-06-15 13:18:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const morgan = require("morgan")
-const connectDB = require("./config/db")
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 // mongodb connection
 connectDB();
@@ -12,27 +12,26 @@ connectDB();
 //Res object
 const app = express();
 
-
 //middlewares
 app.use(cors());
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan("dev"));
 
 //routes
-app.get('/',(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"Welcome to full stack app mahesh"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to full stack app",
+  });
+});
 
-app.use('/api/v1/auth',require('./routes/userRoutes'))
+app.use("/api/v1/auth", require("./routes/userRoutes"));
 
 //Port
 const PORT = process.env.PORT || 8080;
 
 //Listen
 
-app.listen(PORT,()=>{
-    console.log(`server running  ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running  ${PORT}`);
+});
