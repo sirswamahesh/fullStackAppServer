cfa3d356908608295a1925b2caa0d4ae56b057df
sirswamahesh <157932821+sirswamahesh@users.noreply.github.com>
2024-06-16 18:41:42 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -110,4 +110,43 @@ const LoginController = async (req, res) => {
   }
 };
 
-module.exports = { registerController, LoginController };
+const updateUserController = async (req, res) => {
+  try {
+    const { name, password, email, _id } = req.body;
+    console.log(name, password, email, _id);
+    //user find
+    const user = await userModel.findOne({ _id });
+    console.log(user);
+    //password validate
+    if (password && password.length < 6) {
+      return res.status(400).send({
+        success: false,
+        message: "Password is required and should be 6 character long",
+      });
+    }
+
+    const updatedUser = await userModel.findOneAndUpdate(
+      { _id },
+      {
+        name: name || user.name,
+        password: password || user.password,
+        email: email || user.email,
+      }
+    );
+
+    res.status(200).send({
+      success: true,
+      message: "Profile Updated Please Login",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In User Update Api",
+      error,
+    });
+  }
+};
+
+module.exports = { registerController, LoginController, updateUserController };
